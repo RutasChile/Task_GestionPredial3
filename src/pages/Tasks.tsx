2cb5b0f277fb635d3484c8491e862e5cf17441e4
RutasChile <rutasdelsurxchile@gmail.com>
2025-05-21 19:50:07 -0400

    Create Tasks page component

---
+++ src/pages/Tasks.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Tasks() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold text-gray-900 mb-6">Tasks</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p className="text-gray-600">Your tasks will appear here.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Tasks;
